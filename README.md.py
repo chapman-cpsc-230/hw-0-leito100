c11f89ab4551b4c7fec3de8f3181269d325c0c1f
leito100 <leito100@mail.chapman.edu>
2016-02-04 14:15:48 -0800

    jhfj

---
+++ README.md.py
@@ -0,0 +1,24 @@
+# Git Workflow
+
+**Author:** _Paloma Leiton_
+
+**Due date:** _\<date\>_
+
+## Description
+
+_\<Give a description of the assignment here. Often this will be copied from the text.\>_
+
+## What I Learned from This
+
+_\<Explain what you learned from this assignment. This should be in your own words.\>_
+
+## Honor Pledge
+
+I pledge that all the work in this repository is my own with only the following exceptions:
+
+* Content of starter files supplied by the instructor;
+* Code borrowed from another source, documented with correct attribution in the code and summarized here.
+
+Signed,
+
+_\<your name\>_
